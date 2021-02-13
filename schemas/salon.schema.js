12176c0c42a0585388cf16f678201ab825b83dc1
Luis FAOS <luisfaos.web@gmail.com>
2021-02-12 23:58:15 -0300

    adding schemas folder

---
+++ schemas/salon.schema.js
@@ -0,0 +1,22 @@
+import * as Yup from "yup"
+
+const validations = Yup.object().shape({
+    name: Yup.string("O nome deve ser uma string.")
+        .trim()
+        .min(3, "Nome do salão muito curto.")
+        .max(50, "Nome do salão muito longo.")
+        .required("Nome do salão é obrigatório."),
+
+    adressNumber: Yup.string("O número deve ser uma string.")
+        .trim()
+        .max(20, "Número muito longo.")
+        .required("Número é obrigatório."),
+        
+    contactNumber: Yup.string("O Tel/Cel deve ser uma string.")
+        .trim()
+        .matches("^((1[1-9])|([2-9][0-9]))((3[0-9]{3}[0-9]{4})|(9[0-9]{3}[0-9]{5}))$",
+        "Tel/cel está inválido.")
+        .required("Tel/cel é obrigatório."),
+})
+
+export default validations
