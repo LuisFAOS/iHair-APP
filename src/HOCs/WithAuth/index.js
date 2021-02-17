d4367b93767bdc4a832434f8d938412515f8d3a7
Luis FAOS <luisfaos.web@gmail.com>
2021-02-17 17:52:14 -0300

    adding auth HOC for private routes (users with normaluser permission)

---
+++ src/HOCs/WithAuth/index.js
@@ -0,0 +1,44 @@
+import { useContext } from 'react';
+import { useRouter } from 'next/router'
+import AuthContext from '../../AuthContext'
+
+import Loading from '../../components/Loading'
+
+const withAuth = Component => {
+    const Auth = props => {
+
+        const { isSigned, loading, authDatas } = useContext(AuthContext);
+        const router = useRouter()
+
+        const LoadingContainer = (
+            <div style={{height: '100vh', display: "flex", justifyContent: 'center', alignItems: 'center'}}>
+                <Loading size="large" color="#DC3545"/>
+            </div>
+        )
+        
+        if (loading) {
+            return LoadingContainer
+        }
+
+        if(!isSigned || authDatas.client.permissionOf === 'normalUser'){
+            router.push({
+                pathname: '/login',
+                query: {
+                    redirect: true
+                }
+            })
+                
+            return LoadingContainer
+        }
+
+        return <Component {...props}/>
+    };
+
+    if (Component.getInitialProps) {
+        Auth.getInitialProps = Component.getInitialProps;
+    }
+
+    return Auth;
+};
+
+export default withAuth;
