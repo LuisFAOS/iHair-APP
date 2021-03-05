a7f9e9dc1b7858d43e0742651a95adb33a1fffc7
Luis FAOS <luisfaos.web@gmail.com>
2021-03-04 23:56:10 -0300

    sending to login unauth users

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,7 +1,8 @@
 import React, {createContext, useEffect, useState} from 'react'
+import { useRouter } from 'next/router'
 
-import deleteAllCookies from './native/cookies/deleteAllCookies'
-import getCookie from './native/cookies/getCookie'
+import deleteAllCookies from './utils/cookies/deleteAllCookies'
+import getCookie from './utils/cookies/getCookie'
 
 const AuthContext = createContext({ isSigned: false, authDatas: {} })
 
@@ -9,34 +10,48 @@ export function AuthProvider({children}) {
 
     const [authDatas, setAuthDatas] = useState(null)
     const [loading, setLoading] = useState(true) 
+
+    const router = useRouter()
     
     useEffect(() => {
-        const client = {
-            ...JSON.parse(getCookie('client')).client,
-            permissionOf: JSON.parse(getCookie('client')).permissionOf
-        }
-        const token = getCookie('authToken')
-        if(client && token){
-            const authOBJ = {
-                client,
-                token
-            } 
-            
-            setAuthDatas({
-                ...authOBJ
-            })
-        }
+        (function getAuthDatasFromCookies(){
+            try{
+                const client = {
+                    ...JSON.parse(getCookie('client')).client,
+                    permissionOf: JSON.parse(getCookie('client')).permissionOf
+                }
+
+                const token = getCookie('authToken')
+                if(client && token){
+                    const authOBJ = {
+                        client,
+                        token
+                    } 
+                    
+                    setAuthDatas({
+                        ...authOBJ
+                    })
+                }
+            }catch(error){
+                const withAuthPages = ['/lista-saloes']
+                if(withAuthPages.includes(window.location.href)){
+                    router.push('/login')
+                }   
+            }
+        })();
+        
         setLoading(false)
     },[])
     
     
     const signOut = () => {
-        deleteAllCookies()
         setAuthDatas(null);
+        deleteAllCookies();
     }
     
     const signIn = ({client, permissionOf, authToken}) => {
-        setAuthDatas({client, permissionOf, authToken})
+        const newState = {client: {name: client.name, permissionOf}, authToken}
+        setAuthDatas({...newState})
 
         const todayDate = new Date()
         todayDate.setTime(todayDate.getTime() + (3 * 24 * 60 * 60 * 1000))         
